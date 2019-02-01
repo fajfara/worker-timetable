ec4dbb28f1eaf2939bc098fa5e399dd3a9c9b03c
fajfara <a.fajfar95@gmail.com>
2019-02-01 23:12:12 +0100

    Updates

--- src/components/Timetable.js
+++ src/components/Timetable.js
@@ -12,7 +12,7 @@ export default class Timetable extends Component {
           </div>
           <div className="container timetable-main-area">
 
-            <div className="row timetable--monday mb-4 pt-4 border-top">
+            <div className="row timetable--legend mb-4 pt-4 border-top sticky">
               <div className="col-2 timetable-main-area--edit border-right">
               <button type="button" class="btn btn-primary">Seznam delavcev</button>
               </div>
@@ -20,12 +20,24 @@ export default class Timetable extends Component {
                 <div className="hours-start">
                   08:30
                 </div>
+                <div className="hours-middle">
+                  15:00
+                </div>
                 <div className="hours-end">
                   22:00
                 </div>
               </div>
             </div>
 
+            <div className="row timetable--explain mb-4 pt-4 border-top">
+              <div className="col-2 timetable-main-area--edit border-right">
+
+              </div>
+              <div className="col-10 hours">
+                <h2>Grafični prikaz dela delavcev</h2>
+              </div>
+            </div>
+            
             <div className="row timetable--monday mb-4 pt-4 border-top">
               <div className="col-2 timetable-main-area--edit border-right">
                 <h3>Ponedeljek</h3>
@@ -37,21 +49,33 @@ export default class Timetable extends Component {
                   <div className="row timetable-main-area--worker mb-2">
                     <div className="timetable-main-area--worker--display worker-one text-center">
                       <p>Anže Fajfar</p>
+                      <div className="timetable-main-area--worker-info-box">
+                        <span>Od 8:30 do 15:00</span>
+                      </div>
                     </div>
                   </div>
                   <div className="row timetable-main-area--worker mb-2">
                     <div className="timetable-main-area--worker--display worker-two text-center">
                       <p>Nal premik</p>
+                      <div className="timetable-main-area--worker-info-box">
+                        <span>Od 9:00 do 15:00</span>
+                      </div>
                     </div>
                   </div>
                   <div className="row timetable-main-area--worker mb-2">
                     <div className="timetable-main-area--worker--display worker-three text-center">
                       <p>Marjan Novak</p>
+                      <div className="timetable-main-area--worker-info-box">
+                        <span>Od 15:00 do 22:00</span>
+                      </div>
                     </div>
                   </div>
                   <div className="row timetable-main-area--worker mb-2">
                     <div className="timetable-main-area--worker--display worker-four text-center">
                       <p>Janez Kovač</p>
+                      <div className="timetable-main-area--worker-info-box">
+                        <span>Od 17:00 do 22:00</span>
+                      </div>
                     </div>
                   </div>
                 </div>
